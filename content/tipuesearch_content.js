687410abbb826777ddda929b509d2c0955af237f
40823203 <40823203@mde.tw>
2019-11-08 01:18:58 +0800

    add graphics files

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '放大   回復   Hello   For Loop   Sum Runge-Kutta \n \n', 'tags': '', 'url': 'About.html'}, {'title': 'blog', 'text': '', 'tags': '', 'url': 'blog.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '放大   回復   Hello   For Loop   Sum Runge-Kutta \n \n', 'tags': '', 'url': 'About.html'}, {'title': 'youtube', 'text': '', 'tags': '', 'url': 'youtube.html'}]};
