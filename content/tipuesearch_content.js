49600c3a93ac8bfb869b777d65af0109fb6a7f25
40823203 <40823203@mde.tw>
2019-11-08 00:49:57 +0800

    add youtube

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '放大   回復   Hello   For Loop   Sum Runge-Kutta \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '放大   回復   Hello   For Loop   Sum Runge-Kutta \n \n', 'tags': '', 'url': 'About.html'}, {'title': 'blog', 'text': '', 'tags': '', 'url': 'blog.html'}]};
