ee9597d16d639f512fdbf01de784f8af997afef0
40823203 <40823203@mde.tw>
2020-01-02 17:16:44 +0800

    add17

---
+++ downloads/dart_ex/cf/cf.dart
@@ -0,0 +1,14 @@
+main(){
+num c=32;
+//浮點數C為32
+num f=32;
+//浮點數F為32
+double k1=(9/5)*c+32;
+//雙浮點數K1為(9/5)*c+32
+double k2=(f-32)*5/9;
+//雙浮點數K2為(f-32)*5/9
+print('The value of x at c=$c is: $k1');
+//推送The value of x at c is: k1的數值
+print('The value of x at f=$f is: $k2');
+//推送The value of x at f is: k2的數值
+}
